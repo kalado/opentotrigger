8286b805e1a337bcc45e8d8a94bc6f7524b218d8
Kalado <kalado@gmail.com>
2011-09-21 22:37:59 -0300

    CRUD inicial utilizando MongoDB + YiiFramework
    Pessoal, ponto de partida para nossa aplicação.
    Um abraço!

---
+++ code/app/protected/views/user/_view.php
@@ -0,0 +1,20 @@
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('_id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->_id), array('view', 'id'=>$data->_id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('login')); ?>:</b>
+	<?php echo CHtml::encode($data->login); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('name')); ?>:</b>
+	<?php echo CHtml::encode($data->name); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('pass')); ?>:</b>
+	<?php echo CHtml::encode($data->pass); ?>
+	<br />
+
+
+</div>
---
+++ code/app/protected/views/user/view.php
@@ -0,0 +1,26 @@
+<?php
+$this->breadcrumbs=array(
+	'Users'=>array('index'),
+	$model->name,
+);
+
+$this->menu=array(
+	array('label'=>'List User', 'url'=>array('index')),
+	array('label'=>'Create User', 'url'=>array('create')),
+	array('label'=>'Update User', 'url'=>array('update', 'id'=>$model->_id)),
+	array('label'=>'Delete User', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->_id),'confirm'=>'Are you sure you want to delete this item?')),
+	array('label'=>'Manage User', 'url'=>array('admin')),
+);
+?>
+
+<h1>View User #<?php echo $model->_id; ?></h1>
+
+<?php $this->widget('zii.widgets.CDetailView', array(
+	'data'=>$model,
+	'attributes'=>array(
+		'login',
+		'name',
+		'pass',
+		'_id',
+	),
+)); ?>
