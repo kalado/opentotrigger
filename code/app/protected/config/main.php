dd431dd93be682e5063aed8c09994391ac2c84fa
Kalado <kalado@gmail.com>
2011-09-22 02:30:15 -0300

    Aplicação do Administrativo no Yii Framework

--- code/app/protected/config/main.php
+++ code/app/protected/config/main.php
@@ -8,6 +8,7 @@
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'Anime Trigger',
+        'theme'=>'trigger',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
