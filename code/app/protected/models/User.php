59c08ea0815f992e0888b8b74c94a3c405cbe2da
cabelin <mmds89@gmail.com>
2011-09-25 08:51:50 -0300

    teste

--- code/app/protected/models/User.php
+++ code/app/protected/models/User.php
@@ -1,6 +1,6 @@
 <?php
 class User extends EMongoDocument
-    {
+{
       public $login;
       public $name;
       public $pass;
@@ -34,5 +34,5 @@ class User extends EMongoDocument
           'pass'   => 'Senha',
         );
       }
-    }
+}
 ?>
