9e77b5469f569d34efecc6e6583fe232b69e8095
Bruno <wadge.motta@gmail.com>
2013-03-28 00:11:38 -0300

    Criando controller de Captulos

---
+++ web/app/Controller/AnimeController.php
@@ -0,0 +1,147 @@
+<?php
+class AnimeController extends AppController{
+    
+    /**
+     * Adiciona ou Edita uma nova anime
+     */
+    private function save($id=NULL,$id_serie=NULL){
+        if(!empty($this->request->data)){
+            $save = $this->Anime->save($this->request->data);
+            if($save!==FALSE){
+                $this->Session->setFlash("O anime foi salvo com sucesso!", 'alert', array(
+                    'plugin' => 'TwitterBootstrap',
+                    'class' => 'alert-success'
+                ));
+                $id = $this->Anime->getInsertID();
+                $id = ((empty($id))?$this->request->data['Anime']['id']:$id); 
+                $this->redirect(array('controller' => $this->name, 'action' => 'edit' , $id ));
+            }else{
+                $this->Session->setFlash("Ocorreu um erro na tentativa de salvar o anime, favor conferir os dados e tentar novamente!", "alert", array(
+                    "plugin" => "TwitterBootstrap",
+                    'class' => 'alert-error'
+                ));
+            }
+        }
+        if($id != ""){
+            $this->request->data = $this->Anime->find('first',array('conditions' => array($this->name.'.id' => $id)));
+            
+            $this->request->data['Anime']['lancamento'] = $this->tratarData($this->request->data['Anime']['lancamento']);
+            $this->request->data['Anime']['finalizacao'] = $this->tratarData($this->request->data['Anime']['finalizacao']);
+            
+            if(!empty ($this->request->data['Criado'])){
+                foreach ( $this->request->data['Criado'] as $autor){
+                    $autores[] = $autor['id'];
+                }
+            }else{
+                $autores = array();
+            }
+            $this->request->data['Anime']['autores'] =  array_map("intval", $autores);
+            
+            
+            if(!empty ($this->request->data['Fansubs'])){
+                foreach ( $this->request->data['Fansubs'] as $fansub){
+                    $fansubs[] = $fansub['id'];
+                }
+            }else{
+                $fansubs = array();
+            }
+            $this->request->data['Anime']['fansubs'] =  array_map("intval", $fansubs);
+                        
+        }
+        if($id!=NULL){
+            $this->page_title = $this->Anime->getField($id,'nome');
+        }else{
+            /**
+             * Nesse caso (um novo anime) os autores vem como os mesmos da série (pode ser mudado)
+             **/
+            /*
+             * No caso de um anime novo, o editor vem com os autores padrões da série
+             */
+            $autores = $this->Serie->getAutores($id_serie);
+            if(!empty($autores) ){
+                foreach ( $autores as $autor){
+                    $autores[] = $autor['Autores']['autor_id'];
+                }
+            }else{
+                $autores = array();
+            }
+            $this->request->data['Anime']['autores'] =  array_map("intval", $autores);
+        }
+        $fildset = (($id==NULL)?"Novo Material":"Editar");
+        $campos = array(
+                $fildset => array(
+                    'id' => array('type'=>'hidden'),
+                    'serie_id' => array('type'=>'hidden','value'=>$id_serie),
+                    
+                    'multimidia_id' => array('label'=>'Multimidia','type'=>'select','options'=>$this->Multimidia->getArraySimples('nome'),'class'=>'input-block-level'),
+                    'nome' => array('required'=>TRUE , 'class'=>'input-block-level'),
+                    'apelido' => array('class'=>'input-block-level'),
+                    'sinopse' => array('type'=>'textarea-editor'),
+                    ),
+                "Detalhes técnicos" => array(
+                    'lancamento' => array('label'=>'Lançamento','class'=>'datePiker'),
+                    'status_id' => array('label'=>'Status','type'=>'select','options'=>$this->Status->getArraySimples('nome')),
+                    'autores' => array('label'=>'Autores' , 'type'=>'checkbox-inline', 'options' => $this->Autor->getArraySimples('nome')),
+                    'fansubs' => array('label'=>'Fansubs' , 'type'=>'checkbox-inline', 'options' => $this->Fansub->getArraySimples('sigla')),
+                    'idioma_id' => array('label'=>'Idioma','type'=>'select','options'=>$this->Idioma->getArraySimples('nome')),
+                )
+            );
+        
+            $this->set(
+                        array(
+                                'model' => 'Anime',
+                                'campos' => $campos,
+                            )
+                    );
+            
+            $this->set($this->Menus->MenuSerieADMIN($id_serie));
+
+            
+    }
+    
+    public function novo($id_serie){
+        $this->beforeAdmin();
+        $this->save(NULL,$id_serie);
+        $this->render('save');
+    }
+    
+    
+    public function edit($id){
+        $this->beforeAdmin();
+        $this->save($id,$this->Anime->getField($id,'serie_id'));
+        $this->render('save');
+    }
+    
+    
+    public function lista(){
+        $this->beforeAdmin();
+        $lista = $this->getPaginate('Anime', array('Anime.nome') , array('id','nome'));
+        $this->set(
+                array(
+                    'fields' => array('#'=>'Anime.id','Nome'=>'Anime.nome'),
+                    'data' => $lista,
+                )
+        );
+    }
+    
+    public function delete($id){
+        $this->beforeAdmin();
+        $nome = $this->Anime->getField($id,'nome');
+        $deletado = $this->Anime->delete($id);
+        if($deletado==TRUE){
+            $this->Session->setFlash("O anime (".$nome.") foi deletada com sucesso!", 'alert', array(
+                    'plugin' => 'TwitterBootstrap',
+                    'class' => 'alert-success'
+                ));            
+        }else{
+            $this->Session->setFlash("O anime (".$nome.") não pode ser deletada!", 'alert', array(
+                    'plugin' => 'TwitterBootstrap',
+                    'class' => 'alert-error'
+                ));            
+        }
+        $this->redirect(array('action' => 'lista'));
+    }
+    
+}
+
+?>
--- web/app/Controller/CapituloController.php
+++ web/app/Controller/CapituloController.php
@@ -1,23 +1,23 @@
 <?php
-class ServidorController extends AppController{
+class CapituloController extends AppController{
     
     /**
-     * Adiciona ou Edita um novo servidor
+     * Adiciona ou Edita um novo capitulo
      */
-    private function save($id=NULL){
+    private function save($id=NULL,$anime_id=NULL){
         if(!empty($this->request->data)){
-            $save = $this->Servidor->save($this->request->data);
+            $save = $this->Capitulo->save($this->request->data);
             
             if($save!==FALSE){
-                $this->Session->setFlash("O Servidor foi salvo com sucesso!", 'alert', array(
+                $this->Session->setFlash("O Capitulo foi salvo com sucesso!", 'alert', array(
                     'plugin' => 'TwitterBootstrap',
                     'class' => 'alert-success'
                 ));
-                $id = $this->Servidor->getInsertID();
-                $id = ((empty($id))?$this->request->data['Servidor']['id']:$id); 
+                $id = $this->Capitulo->getInsertID();
+                $id = ((empty($id))?$this->request->data['Capitulo']['id']:$id); 
                 $this->redirect(array('controller' => $this->name, 'action' => 'edit' , $id ));
             }else{
-                $this->Session->setFlash("Ocorreu um erro na tentativa de salvar o servidor, favor conferir os dados e tentar novamente!", 'alert', array(
+                $this->Session->setFlash("Ocorreu um erro na tentativa de salvar o capitulo, favor conferir os dados e tentar novamente!", 'alert', array(
                     'plugin' => 'TwitterBootstrap',
                     'class' => 'alert-error'
                 ));
@@ -26,33 +26,32 @@ class ServidorController extends AppController{
         }
         
         if($id != ""){
-            $this->request->data = $this->Servidor->find('first',array('conditions' => array('id' => $id) , 'fields'=>array('id','nome','habilitado','usuario','senha')));
+            $this->request->data = $this->Capitulo->find('first',array('conditions' => array('id' => $id) , 'fields'=>array('id','nome','habilitado','usuario','senha')));
+        }else{
+            $this->request->data['Capitulo']['anime_id'] = $anime_id;
         }
-        if($id!=NULL)$this->page_title = $this->Servidor->getField($id,'nome');
-        $fildset = (($id==NULL)?"Novo Servidor":"Editar servidor");
+        if($id!=NULL)$this->page_title = $this->Capitulo->getField($id,'nome');
+        $fildset = (($id==NULL)?"Novo Capitulo":"Editar capitulo");
         $campos = array(
                 $fildset => array(
                     'id' => array('type'=>'hidden'),
-                    'nome' => array('label'=>'Nome','required'=>TRUE),
-                    'habilitado' => array('type'=>'radio' , 'options'=>array('1' => 'Ativado', '0' => 'Desativado') , 'value'=>'1'),
-                ),
-                'Adicionais' => array(
-                    'usuario' => array('label'=>'Usuário'),
-                    'senha' => array(),
+                    'anime_id' => array('type'=>'hidden'),
+                    'titulo' => array('required'=>TRUE),
+                    'numero' => array('label'=>'Número'),
                 )
             );
         
             $this->set(
                         array(
-                                'model'=>'Servidor',
+                                'model'=>'Capitulo',
                                 'campos' => $campos
                             )
                     );
     }
     
-    public function novo(){
+    public function novo($anime_id){
         $this->beforeAdmin();
-        $this->save();
+        $this->save(NULL,$anime_id);
         $this->render('save');
     }
     
@@ -60,33 +59,34 @@ class ServidorController extends AppController{
     public function edit($id){
         $this->beforeAdmin();
         $this->save($id);
+        
         $this->render('save');
     }
     
     
     public function lista(){
         $this->beforeAdmin();
-        $lista = $this->getPaginate('Servidor', array('Servidor.nome') , array('id','nome','habilitado'));
+        $lista = $this->getPaginate('Capitulo', array('Capitulo.nome') , array('id','nome','habilitado'));
         $this->set(
                 array(
-                    'fields' => array('#'=>'Servidor.id','Nome'=>'Servidor.nome' , 'Ativado' => 'Servidor.habilitado'),
+                    'fields' => array('#'=>'Capitulo.id','Nome'=>'Capitulo.nome' , 'Ativado' => 'Capitulo.habilitado'),
                     'data' => $lista,
-                    'virtualFields' => array("Servidor.habilitado"=>array("1"=>"Sim","0"=>"Não")),
+                    'virtualFields' => array("Capitulo.habilitado"=>array("1"=>"Sim","0"=>"Não")),
                 )
         );
     }
     
     public function delete($id){
         $this->beforeAdmin();
-        $nome = $this->Servidor->getField($id,'nome');
-        $deletado = $this->Servidor->delete($id);
+        $nome = $this->Capitulo->getField($id,'nome');
+        $deletado = $this->Capitulo->delete($id);
         if($deletado==TRUE){
-            $this->Session->setFlash("O servidor (".$nome.") foi deletado com sucesso!", 'alert', array(
+            $this->Session->setFlash("O capitulo (".$nome.") foi deletado com sucesso!", 'alert', array(
                     'plugin' => 'TwitterBootstrap',
                     'class' => 'alert-success'
                 ));            
         }else{
-            $this->Session->setFlash("O servidor (".$nome.") não pode ser deletado!", 'alert', array(
+            $this->Session->setFlash("O capitulo (".$nome.") não pode ser deletado!", 'alert', array(
                     'plugin' => 'TwitterBootstrap',
                     'class' => 'alert-error'
                 ));            
