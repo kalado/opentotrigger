ddaf0d4963e573bfabf59c06b8b0e89f1db8fdb2
Bruno <wadge.motta@gmail.com>
2013-03-30 05:07:16 -0300

    Criando função de busca de qualidades aceitas

--- web/app/Model/Multimidia.php
+++ web/app/Model/Multimidia.php
@@ -46,14 +46,15 @@ class Multimidia extends AppModel{
     }
     
     public function getQualidadesAceitas($id){
-        $qualidades = $this->query(
-                    'SELECT * FROM compativel AS Compativel '.
-                        'WHERE '.
-                            'Compativel.multimidia_id='.$id.
-                        ' ;');
-        
-        return $qualidades;
+        $qualidades = $this->find('first',array('conditions' => array($this->name.'.id' => $id )));
+        $qualidades_aceitas_options = array();
+        foreach($qualidades['Qualidades'] as $qualidade){
+            $qualidades_aceitas_options[$qualidade['id']]=$qualidade['sigla'];
+        }
+        return $qualidades_aceitas_options;
     }
+
+    
     
 }
 ?>
